eb10db66c230b840267317e37de5bfe6ff9db896
Gyanender Singh <gyanendersingh5@gmail.com>
2021-01-25 20:23:32 +0530

    Fixed cors issue for json

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,7 @@
 let MESSAGES = {};
 export const setMsgJson = async (setIsLoading) => {
   const fetchOptions = {
-    method: "GET",
     cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-cache",
-    },
   };
   MESSAGES = await fetch(`${window.FILE_PATH}/msg.json`, fetchOptions).then((resp) => resp.json());
   setIsLoading(false);
