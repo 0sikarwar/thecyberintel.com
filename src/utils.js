c6dfcab53e7c7afac9a975a3fb147c97ce289b23
Gyanender Singh <gyanendersingh5@gmail.com>
2021-01-25 20:10:46 +0530

    Added no cache in header

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,14 @@
 let MESSAGES = {};
 export const setMsgJson = async (setIsLoading) => {
-  MESSAGES = await fetch(`${window.FILE_PATH}/msg.json`).then((resp) => resp.json());
+  const fetchOptions = {
+    method: "GET",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache",
+    },
+  };
+  MESSAGES = await fetch(`${window.FILE_PATH}/msg.json`, fetchOptions).then((resp) => resp.json());
   setIsLoading(false);
 };
 export const getMsgJson = () => ({ ...MESSAGES });
