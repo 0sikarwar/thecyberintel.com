d95dbf332bf6c722279b5c1501dd6ff3a5f84fd6
Gyanender Singh <gyanendersingh5@gmail.com>
2024-12-26 23:04:23 +0530

    added axios interceptor for auth token

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 
-export default axios.create({
-  timeout: 120000,
-  withCredentials: true,
-});
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      config.headers["x-auth"] = token;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    const token = response.headers["x-auth"];
+    if (token) {
+      localStorage.setItem("auth_token", token);
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
